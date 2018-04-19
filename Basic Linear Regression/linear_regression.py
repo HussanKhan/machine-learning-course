acbe9df1eb74c001c48367cccadfd160f0f8a374
Hussan Khan <hussanrkhan95@gmail.com>
2018-04-19 13:21:03 -0500

    Prepped and split data

--- Basic Linear Regression/linear_regression.py
+++ Basic Linear Regression/linear_regression.py
@@ -7,8 +7,32 @@ import seaborn as sns
 """
     Main equation:
     y = b + mx
-    (dependent) = (multiplier of rate) + (rate)(independent)
+    (dependent) = (minimum point) + (rate of change)(independent)
+
+    for each point we do this (Total sum for each point)
+    (y-(mx + b))^2 + ... (yn - (mx+b))^2
+
+    After summing we get total errors
+    Now we adjust m and b and run again, until our total error is lowest
+    (usally most occuring errorsum)
 """
 
-# Importing the dataset
+# Importing the dataset and preproces
 dataset = pd.read_csv('Salary_Data.csv')
+
+#See which data is dependent and independent, we try to guess dependent
+# print(dataset.head())
+
+#Looks like Salary is dependent on years experiance, remember to extract values for
+# 2-dim array. Remember the points are linked by index
+X = dataset['YearsExperience'].values
+y = dataset['Salary'].values
+# print(X)
+# print(y)
+
+# Now split into training set and testing set
+from sklearn.model_selection import train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=32, test_size=0.2)
+
+# Since there are only two features, we don't need to worry about feature scaling
